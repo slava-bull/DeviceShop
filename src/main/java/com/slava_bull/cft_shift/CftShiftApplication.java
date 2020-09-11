ca62f4b017c97b7fd81717a4b96c728c1ac97a2b
Slava-bull <slava-bull@mail.ru>
2020-09-11 13:19:25 +0700

    init app

---
+++ src/main/java/com/slava_bull/cft_shift/CftShiftApplication.java
@@ -0,0 +1,13 @@
+package com.slava_bull.cft_shift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CftShiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CftShiftApplication.class, args);
+    }
+
+}
